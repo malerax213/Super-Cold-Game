b99ce77155f836afb9fb7e29f8a98140f3771d12
Alexandru Martinas <alexandrumartinas33@gmail.com>
2019-12-04 20:04:51 +0100

    Different maps and music addded

---
+++ SuperCOLDGame/Assets/Scripts/NextScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class NextScript2 : MonoBehaviour
+{
+	public void NextGame2()
+	{
+		SceneManager.LoadScene("Map3", LoadSceneMode.Single);
+	}
+}
---
+++ SuperCOLDGame/Assets/Scripts/NextScript2.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class NextScript : MonoBehaviour
+{
+	public void NextGame()
+	{
+		SceneManager.LoadScene("Map2", LoadSceneMode.Single);
+	}
+}
